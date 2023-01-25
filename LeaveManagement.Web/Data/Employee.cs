c98f8e09e804bd4cfc5a929339e2aca285178fa3
Edward Güereque <edwardgnt@gmail.com>
2023-01-24 23:40:12 -0700

    Added database tables and migrations

---
+++ LeaveManagement.Web/Data/Employee.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagement.Web.Data
+{
+    // Extending the AspNetUsers Table
+    public class Employee : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? TaxId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
+    
