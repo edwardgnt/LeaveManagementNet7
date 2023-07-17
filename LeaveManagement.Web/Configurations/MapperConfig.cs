94167795d9fd9752c08d65ca96ce50f984635c0f
edwardgnt@gmail.com <edwardgnt@gmail.com>
2023-07-17 17:47:37 -0600

    Adjusted Edit and Details page

---
+++ LeaveManagement.Web/Configurations/MapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Models;
+
+namespace LeaveManagement.Web.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            // Convert from Data Model into View Model 
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number of Days")]
+        [Range(1, 25, ErrorMessage = "Please Enter a Valid Number")]
+        [Required]
+        public int DefaultDays { get; set; }
+    }
+}
